356d3d6b09031b78582dad1e37ba3b07528c8001
Snehitha <lakkurusnehitha@gmail.com>
2025-06-30 21:55:47 +0530

    Delete project files/axiosinstance.js

--- project files/axiosinstance.js
+++
@@ -1,9 +0,0 @@
-// src/Helpers/axiosinstance.js
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "https://lms-server-vd61.onrender.com/api/v1", // ✅ Corrected
-  withCredentials: true, // ✅ Important for cookies/session
-});
-
-export default axiosInstance;
